99e80c89433207de3bc63027238553efd6bf8ab7
Keen1949 <472912459@qq.com>
2023-01-17 10:52:32 +0800

    初始版本上传

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace DecodeHelperApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.selectButton = new System.Windows.Forms.Button();
+            this.targetButton = new System.Windows.Forms.Button();
+            this.changeButton = new System.Windows.Forms.Button();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.selectedPathTextBox = new System.Windows.Forms.TextBox();
+            this.targetPathTextBox = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // selectButton
+            // 
+            this.selectButton.Location = new System.Drawing.Point(17, 44);
+            this.selectButton.Name = "selectButton";
+            this.selectButton.Size = new System.Drawing.Size(95, 23);
+            this.selectButton.TabIndex = 0;
+            this.selectButton.Text = "原文件路径";
+            this.selectButton.UseVisualStyleBackColor = true;
+            this.selectButton.Click += new System.EventHandler(this.SelectButton_Click);
+            // 
+            // targetButton
+            // 
+            this.targetButton.Location = new System.Drawing.Point(17, 99);
+            this.targetButton.Name = "targetButton";
+            this.targetButton.Size = new System.Drawing.Size(95, 23);
+            this.targetButton.TabIndex = 1;
+            this.targetButton.Text = "保存路径";
+            this.targetButton.UseVisualStyleBackColor = true;
+            this.targetButton.Click += new System.EventHandler(this.TargetButton_Click);
+            // 
+            // changeButton
+            // 
+            this.changeButton.Location = new System.Drawing.Point(161, 150);
+            this.changeButton.Name = "changeButton";
+            this.changeButton.Size = new System.Drawing.Size(111, 33);
+            this.changeButton.TabIndex = 2;
+            this.changeButton.Text = "开始解密";
+            this.changeButton.UseVisualStyleBackColor = true;
+            this.changeButton.Click += new System.EventHandler(this.ChangeButton_Click);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Location = new System.Drawing.Point(15, 195);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(428, 23);
+            this.statusLabel.TabIndex = 3;
+            this.statusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // selectedPathTextBox
+            // 
+            this.selectedPathTextBox.Location = new System.Drawing.Point(118, 45);
+            this.selectedPathTextBox.Name = "selectedPathTextBox";
+            this.selectedPathTextBox.Size = new System.Drawing.Size(329, 21);
+            this.selectedPathTextBox.TabIndex = 4;
+            // 
+            // targetPathTextBox
+            // 
+            this.targetPathTextBox.Location = new System.Drawing.Point(118, 100);
+            this.targetPathTextBox.Name = "targetPathTextBox";
+            this.targetPathTextBox.Size = new System.Drawing.Size(329, 21);
+            this.targetPathTextBox.TabIndex = 5;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(17, 221);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(428, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 254);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.targetPathTextBox);
+            this.Controls.Add(this.selectedPathTextBox);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.changeButton);
+            this.Controls.Add(this.targetButton);
+            this.Controls.Add(this.selectButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "解密助手";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button selectButton;
+        private System.Windows.Forms.Button targetButton;
+        private System.Windows.Forms.Button changeButton;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.TextBox selectedPathTextBox;
+        private System.Windows.Forms.TextBox targetPathTextBox;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DecodeHelperApplication1
+{
+    public partial class Form1 : Form
+    {
+        private FolderBrowserDialog folderBrowser;
+        private FolderBrowserDialog targetFolderBrowser;
+        string newExtension = ".ini";
+        /// <summary>
+        /// 构造方法
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+            folderBrowser = new FolderBrowserDialog();
+            targetFolderBrowser = new FolderBrowserDialog();
+            
+        }
+        /// <summary>
+        /// 原文件路径选择
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SelectButton_Click(object sender, EventArgs e)
+        {
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                selectedPathTextBox.Text = folderBrowser.SelectedPath;
+            }
+        }
+        /// <summary>
+        /// 保存路径选择
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TargetButton_Click(object sender, EventArgs e)
+        {
+            if (targetFolderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                targetPathTextBox.Text = targetFolderBrowser.SelectedPath;
+            }
+        }
+        /// <summary>
+        /// 开始解密
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ChangeButton_Click(object sender, EventArgs e)
+        {
+            //判断是否已经选择原文件路径
+            if (string.IsNullOrEmpty(selectedPathTextBox.Text))
+            {
+                MessageBox.Show("请先选择原文件路径,再进行解密操作！");
+                return;
+            }
+            //判断是否已经选择文件保存路径
+            if (string.IsNullOrEmpty(targetPathTextBox.Text))
+            {
+                MessageBox.Show("请先选择保存路径,再进行解密操作！");
+                return;
+            }
+
+            string sourceDirectoryPath = folderBrowser.SelectedPath;
+            string targetDirectoryPath = targetFolderBrowser.SelectedPath;
+            string[] files = Directory.GetFiles(sourceDirectoryPath, "*");
+            progressBar1.Maximum = files.Length;
+            int i = 0;
+            foreach (string file in files)
+            {
+                i++;
+                //目标文件路径+文件名+.ini
+                string newFilePath = Path.ChangeExtension(Path.Combine(targetDirectoryPath, Path.GetFileName(file)), newExtension);
+                //目标文件路径+文件名+原文件后缀
+                string newFilePath2 = Path.Combine(targetDirectoryPath, Path.GetFileName(file));
+
+                //判断保存路径下是否已经存在要解密的文件
+                if (File.Exists(newFilePath) || File.Exists(newFilePath2))
+                {
+                    DialogResult result = MessageBox.Show("文件"+ Path.GetFileName(file) + "已存在，是否覆盖?", "Confirmation", MessageBoxButtons.YesNo);
+                    if (result == DialogResult.Yes)
+                    {
+                        //解密操作
+                        Decode(file, targetDirectoryPath);
+
+                    }
+                    else
+                    {
+                        statusLabel.Text = "文件 " + Path.GetFileName(file) + " 未解密.";
+                    }
+                }
+                else
+                {
+                    //解密操作
+                    Decode(file, targetDirectoryPath);
+                }
+                progressBar1.Value = i;
+            }
+            MessageBox.Show("文件全部解密完成！");
+        }
+        /// <summary>
+        /// 解密方法
+        /// </summary>
+        /// <param name="file">需要解密的原文件</param>
+        /// <param name="targetDirectoryPath">解密后文件的保存位置</param>
+        public void Decode(string file, string targetDirectoryPath)
+        {
+            //保存原文件的信息，用于还原原文件的后缀
+            string originalFilePath = file;
+            //目标文件路径+文件名+.ini
+            string newFilePath = Path.ChangeExtension(Path.Combine(targetDirectoryPath, Path.GetFileName(file)), newExtension);
+            //将原文件复制一份到保存路径下
+            File.Copy(file, Path.Combine(targetDirectoryPath, Path.GetFileName(file)), true);
+
+            //将保存路径下文件的后缀改为.ini
+            try
+            {
+                File.Move(Path.Combine(targetDirectoryPath, Path.GetFileName(file)), newFilePath);
+                statusLabel.Text = "文件 " + Path.GetFileName(file) + " 成功复制到" + targetDirectoryPath;
+            }
+            catch (Exception)
+            {
+                statusLabel.Text = Path.GetFileName(file) + " 被异常占用！";
+                throw;
+            }
+
+            //延时1000ms
+            Thread.Sleep(1000);
+
+            //将文件后缀改回原来的
+            try
+            {
+                File.Move(newFilePath, Path.Combine(targetDirectoryPath, Path.GetFileName(originalFilePath)));
+                statusLabel.Text = "文件 " + Path.GetFileName(file) + " 成功解密!";
+            }
+            catch (Exception)
+            {
+                statusLabel.Text = Path.GetFileName(file) + " 解密失败！";
+                throw;
+            }
+        }
+    }
+}
